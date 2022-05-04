eb350c330400623646952b8e18db846797225f0d
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-05-04 02:19:43 -0300

    Bootstrap Added

--- src/components/HeaderLoco.js
+++ src/components/HeaderLoco.js
@@ -1,9 +1,12 @@
-import React from 'react';
+
 
 export const HeaderLoco = () => {
+    let nombre = 'Fernando Schwalb';
+
     return (
         <div>
-            <h1>Hola Perris</h1>
+            <h1>Hola Perris, mi nombre es { nombre }</h1>
+            <button className="btn btn-outline-primary my-3">Pinchame</button>
         </div>
     );
 };
